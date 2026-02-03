f7a0f8d8a1ef56d22e349457fcc5cdb3a6c6ccb7
djulou <dorian.julou@gmail.com>
2026-02-03 14:22:33 +0100

    route python

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine
-from models import Album
+from models import Album, Track, User, Playlist
 
 import uvicorn
 import os
@@ -27,6 +27,24 @@ def get_db():
 @app.get("/album") 
 def get_all_albums(db: Session = Depends(get_db)):
     return db.query(Album).all()
+
+@app.get("/track") 
+def get_all_track(db: Session = Depends(get_db)):
+    return db.query(Track).all()
+
+@app.get("/playlist") 
+def get_all_track(db: Session = Depends(get_db)):
+    return db.query(Playlist).all()
+
+
+@app.get("/user/{email}")
+def get_user_by_email(email: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.user_email == email).first()
+    
+    if not user:
+        raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+    
+    return user
 
 origins = [
     "http://localhost:5173",
