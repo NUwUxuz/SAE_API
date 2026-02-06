727ced4623353136d68f900b73fad1e75b4fa680
NUwUxuz <135438037+NUwUxuz@users.noreply.github.com>
2026-02-06 11:41:12 +0100

    Update detail_compte.tsx

--- frontend/mon-projet/src/detail_compte.tsx
+++ frontend/mon-projet/src/detail_compte.tsx
@@ -1,5 +1,6 @@
 import "./user.css"
 import InfoUser from "./components/info_user";
+import StatsUser from "./components/stats_user";
 
 export default
 
@@ -12,7 +13,15 @@ function detail_compte() {
                 password="motdepass123"
             />
 
+            <StatsUser 
+                favoris={35}
+                playlists={12}
+                track_listen={[{user_id:1, track_id:6, nb_listening:27}]}
+                genre_listen={[{user_id:1, genre_id:3, genre_rate:0.24}]}
+            />
+
             <button id="bouton-user" className="bouton-cliquable">Afficher stats utilisateurs</button>
+            <button id="bouton-user" className="bouton-cliquable" hidden>Cacher stats utilisateurs</button>
 
             <div id="bouton-footer">
                 <button id="deconnecter" className="bouton-cliquable">Se déconnecter</button>
